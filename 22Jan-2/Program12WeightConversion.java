a9c9c282f93852272bc9649d00bb63f6ddbfadd0
Bhoovan Kumar <bhoovan2310@gmail.com>
2025-01-22 14:53:49 +0530

    22Jan Assignment 2 Done git add .

---
+++ 22Jan-2/Program12WeightConversion.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Program12WeightConversion {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter weight in pounds: ");
+        double pounds = input.nextDouble();
+
+        double kilograms = pounds / 2.2;
+
+        System.out.println("The weight of the person in pounds is " + pounds + " and in kilograms is " + kilograms);
+    }
+}
