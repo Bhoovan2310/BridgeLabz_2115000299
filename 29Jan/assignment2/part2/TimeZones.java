72ff6739465475273589d90041b3286cad1595cf
Bhoovan Kumar <bhoovan2310@gmail.com>
2025-01-29 11:58:38 +0530

    29Jan Assignment2 Part2 Questions Done

---
+++ 29Jan/assignment2/part2/TimeZones.java
@@ -0,0 +1,10 @@
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+
+public class TimeZones {
+    public static void main(String[] args) {
+        System.out.println(ZonedDateTime.now(ZoneId.of("GMT")));
+        System.out.println(ZonedDateTime.now(ZoneId.of("Asia/Kolkata")));
+        System.out.println(ZonedDateTime.now(ZoneId.of("America/Los_Angeles")));
+    }
+}
